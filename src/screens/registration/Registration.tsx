e4e239534d7b4d21674ce7cb4f212db208c31475
haduigon <haduigon@gmail.com>
2024-11-27 16:21:43 +0200

    Created a project, started setup router

---
+++ src/screens/registration/Registration.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Registration() {
+  return (
+    <View style={styles.box}>
+      <Text>Registratfbdgbion</Text>
+      <Text>Registratfbdgbion</Text>
+      <Text>Registratfbdgbion</Text>
+      <Text>Registratfbdgbion</Text>
+      <Text>Registratfbdgbion</Text>
+      <Text>Registratfbdgbion</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  box: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
