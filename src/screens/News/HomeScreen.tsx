92a919896c8deb52f696fbdfec3beda88f671555
haduigon <haduigon@gmail.com>
2024-11-30 22:41:04 +0200

    Tab navigation seems done.

--- src/screens/News/HomeScreen.tsx
+++ src/screens/News/HomeScreen.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native';
-import Ionicons from 'react-native-vector-icons/FontAwesome';
+// import Ionicons from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/SimpleLineIcons';
 
 export default function HomeScreen(): React.JSX.Element {
   return (
     <View style={styles.box}>
       <Text>Home Screen News</Text>
-    <Ionicons name="glass" size={30} color={'blue'} />
+      {/* <Ionicons name="glass" size={30} color={'blue'} /> */}
+      <Ionicons name="plane" size={22} />
     </View>
   )
 }
